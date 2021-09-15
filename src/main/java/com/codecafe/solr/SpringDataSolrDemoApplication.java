6700d95fa5a5db5f09986e92eb382c1237172e7a
Abhinav Nath <abhinavnath@ymail.com>
2021-09-15 23:57:22 +0530

    Initialize project

---
+++ src/main/java/com/codecafe/solr/SpringDataSolrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSolrDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataSolrDemoApplication.class, args);
+	}
+
+}
