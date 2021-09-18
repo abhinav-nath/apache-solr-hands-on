e602d6eec121216b8f010d2c67715c4602e87e7d
Abhinav Nath <abhinavnath@ymail.com>
2021-09-18 22:16:25 +0530

    Add notes about solr data import handler

---
+++ notes/solr-commands.md
@@ -0,0 +1,10 @@
+## List of useful Solr commands
+
+* Start Solr on local
+
+  `./solr start`
+
+
+* Stop Solr
+
+  `./solr stop`
--- src/main/java/com/codecafe/solr/persistence/entity/AbstractProduct.java
+++ src/main/java/com/codecafe/solr/persistence/entity/AbstractProduct.java
@@ -19,6 +19,6 @@ public class AbstractProduct {
 
     private String brand;
     private String description;
-    private LocalDate dateAdded = LocalDate.now();
+    private final LocalDate dateAdded = LocalDate.now();
 
 }
